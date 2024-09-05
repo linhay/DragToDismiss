813866dba787a12a03883c4e37c41a464b262859
linhey <is.linhey@outlook.com>
2024-09-05 16:52:30 +0800

    chore: 添加本地调试

--- Sources/Package.swift
+++ Sources/Package.swift
@@ -15,7 +15,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "DragToDismiss"),
+        .target(name: "DragToDismiss", path: "DragToDismiss"),
     ]
 )
